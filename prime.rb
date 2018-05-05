db13122ff93afea87b855b2a5a510e6c34c3e0df
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-05 17:58:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,27 +1,29 @@
 def prime?(num)
   if num <= 0 
     return false
-  end 
-  test_num = Math.sqrt(num).floor
-  test_array = (1..test_num).to_a
-  index = 0
-  test_count = 0
-  
-  if num.class == Fixnum
-    while index < test_array.size do
-      if (num % test_array[index]) === 0
-        "Not a prime number"
-      else
-        test_count+=1
-      end
-      index+=1
-    end
-    if test_count == test_array.size
-      true
-    else 
-      false
-    end
+  elsif num == 1 || num == 2 || num == 3
+    return true
   else 
-  return false
-end
+    test_num = Math.sqrt(num).floor
+    test_array = (1..test_num).to_a
+    index = 0
+    test_count = 0
+    if num.class == Fixnum
+      while index < test_array.size do
+        if (num % test_array[index]) == 0
+          "Not a prime number"
+        else
+          test_count+=1
+        end
+      index+=1
+      end
+      if test_count == test_array.size
+        true
+      else 
+        false
+      end
+    else 
+    return false
+    end
+  end 
 end
