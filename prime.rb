124b1a4e158a82a2c87989bbabdfcdb21722f27a
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-05 18:17:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,30 @@
 def prime?(num)
-  if num <= 0 
+  case 
+  when num<=0
     return false
-  elsif num == 1 || num == 2 || num == 3
+  when num == 1
+    return true
+  when num == 2
+    return true
+  when num == 3
+    return true
+  when num == 5
     return true
   else 
     test_num = Math.sqrt(num).floor
     test_array = (1..test_num).to_a
     index = 0
     test_count = 0
-    if num.class == Fixnum
+    if num.is_a? Integer
       while index < test_array.size do
         if (num % test_array[index]) == 0
           "Not a prime number"
         else
           test_count+=1
         end
-      index+=1
+        index+=1
       end
-      if test_count == test_array.size
+      if test_count+1 == test_array.size
         true
       else 
         false
